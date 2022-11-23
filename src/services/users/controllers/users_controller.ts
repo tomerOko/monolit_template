f37f7117be8a1faaa6afdc5e0861b8fa09ad651f
toemro <tomer0@rapyd.net>
2022-11-23 02:16:20 +0000

    logic

--- src/services/users/controllers/users_controller.ts
+++ src/services/users/controllers/users_controller.ts
@@ -1,19 +1,32 @@
 import { Request, Response } from "express"
-import {v4 as genereateID} from 'uuid'
+import {v4 as genereateID, v4} from 'uuid'
 import { wrap } from "../../../utilities/functionWrapping"
+import { UserService } from "../service/users_service"
+import { User } from "../types/users_types"
 
 type This = InstanceType<typeof UserController>
 
 export class UserController {
 
-    constructor(){}
+    constructor(
+        private user_serivce = new UserService()
+    ){}
 
     public async createUser(req: Request, res: Response):Promise<void>{
     await wrap<This['createUser']>(async(req, res) =>{
 
-        //validate params
-        //call service
-        //parse response to respose format
+        const user:User={
+            token: v4(),
+            communities: [],
+            country: req.body.country,
+            name: req.body.name,
+            email: req.body.email,
+            image: req.body.image,
+            role: req.body.role
+        }
+        //todo: validate params
+        this.user_serivce.createUser(user)
+        //todo: build some pre-sending response parsing mechanism
 
     },[req, res], 'UserController/createUser')}
 
