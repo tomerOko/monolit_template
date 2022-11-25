3a7268ddcab2e009a4b7ff3f18392fb0c1644f9b
toemro <tomer0@rapyd.net>
2022-11-25 22:42:22 +0000

    cruds

--- src/services/users____/logic/services/delete_user.ts
+++ src/services/users____/logic/services/delete_user.ts
@@ -10,14 +10,9 @@ export class DeleteUser extends UserLogic {
     constructor() {super()}
 
     public deleteUser = async (user: User):Promise<void> => {
-    await wrap<This["createUser"]>({name:"CreateUser/createUser"}, async()=>{
-        if(user.email) await this.validateMailNotExist(user.email)
+    await wrap<This["deleteUser"]>({name:"DeleteUser/deleteUser"}, async()=>{
         const query_result = await DeleteUser.user_dal.createUser(user)
     })}
 
-    public validateMailNotExist = async (email: string):Promise<void> => {
-    return await wrap<This["validateMailNotExist"]>({name:"UserService/validateMailNotExist"}, async()=>{
-        const email_exist = await DeleteUser.user_dal.getSinlgeUserBy({email})
-        if (email_exist) throw new Error("email allready exist in the system");
-    })}
+
 }
--- src/services/users____/logic/services/update_user_changable_properties.ts
+++ src/services/users____/logic/services/update_user_changable_properties.ts
@@ -4,14 +4,14 @@ import { User } from "../../types/users_types";
 import { UserUtils } from "../../utilities/users_utils";
 import { UserLogic } from "../base_users_logic_class";
 
-type This = InstanceType<typeof CreateUser>
+type This = InstanceType<typeof UpdateUserChangebleProperties>
 
-export class CreateUser extends UserLogic {
+export class UpdateUserChangebleProperties extends UserLogic {
 
     constructor() {super()}
 
-    public createUser = async (user: User):Promise<void> => {
-    await wrap<This["createUser"]>({name:"CreateUser/createUser"}, async()=>{
+    public UpdateUserChangebleProperties = async (user: User):Promise<void> => {
+    await wrap<This["UpdateUserChangebleProperties"]>({name:"UpdateUserChangebleProperties/UpdateUserChangebleProperties"}, async()=>{
         if(user.email) await UserUtils.validateMailNotExist(user.email)
         const query_result = await CreateUser.user_dal.createUser(user)
     })}
