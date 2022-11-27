9ab510c2645e02f78a97738517fc7499e21f907e
toemro <tomer0@rapyd.net>
2022-11-28 01:21:10 +0200

    email exist error handling

--- src/services/users____/utilities/users_utils.ts
+++ src/services/users____/utilities/users_utils.ts
@@ -1,3 +1,4 @@
+import { create_error } from "../../../errors/error_factory";
 import { wrap, wrapSync } from "../../../utilities/function_wrapping";
 import { UserDAL } from "../dal/users_dal";
 
@@ -9,7 +10,7 @@ export class UserUtils {
     public static validateMailNotExist = async (email: string):Promise<void> => {
     return await wrap<This["validateMailNotExist"]>({name:"UserUtils/validateMailNotExist"}, async()=>{
         const email_exist = await this.user_dal.getSinlgeUserBy({email})
-        if (email_exist) throw new Error("email allready exist in the system");
+        if (email_exist) throw create_error("email allready exist error");
     })}
 
 }
