38fb5183fad9d7aea576a3593fb59ddda399d57e
Tomer Oko <49371078+tomerOko@users.noreply.github.com>
2022-12-10 21:41:03 +0000

    deleted communities and posts

--- src/services/communities/logic/base_communities_logic_class.ts
+++
@@ -1,8 +0,0 @@
-import { CommunityDAL } from "../dal/communities_dal";
-import {community_helpers} from './helpers/communities_helpers_index'
-
-export class CommunityLogic{
-    constructor(){}
-    static community_dal = new CommunityDAL();
-    static community_helper = community_helpers
-}
--- src/services/posts/logic/helpers/user_helpers_index.ts
+++
@@ -1,6 +0,0 @@
-import { PostRoleValidator } from "./moderator_validator";
-
-const post_role_validator = new PostRoleValidator()
-
-export const post_helpers = {post_role_validator}
-
