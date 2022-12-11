dec97d4091efb8b053fa1833f2466578370a6956
Tomer Oko <49371078+tomerOko@users.noreply.github.com>
2022-12-11 16:27:52 +0200

    add test db

--- scripts/start_test.sh
+++ scripts/start_test.sh
@@ -1,5 +1,5 @@
 sudo chown -R $USER:$(id -gn $USER) ./* # this give permission to windows to eddit the content of the folder this script runing from, so it lets vscode edit the content of this folder
 sudo docker-compose down
-sudo docker-compose --env-file ./compose.dev.env up -d
+sudo docker-compose --env-file ./compose.test.env up -d
 docker exec -it backend.container bash
 
